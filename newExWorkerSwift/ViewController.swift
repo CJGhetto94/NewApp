d3e00d315d485c7652779ad29487c6e1ebe281d6
CJGhetto94 <cjghetto2013@gmail.com>
2025-05-14 20:59:05 +0300

    ex 12 el-ts of interface

--- newExWorkerSwift/ViewController.swift
+++ newExWorkerSwift/ViewController.swift
@@ -1,21 +1,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var helper = Helper()
+    private let helper = Helper()
+    private let textLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(ciColor: .gray)
         updateNumbers()
+        setupLabel()
+        
+        view.addSubview(textLabel)
         
     }
     private func updateNumbers() {
-        
         helper.addNumber(Int.random(in: 1...10))
-        
-        for number in helper.getNumbers() {
-            print(number)
-        }
+    }
+    
+    private func setupLabel() {
+        let firstNumber = helper.getNumbers().first
+        textLabel.text = "\(firstNumber ?? 0)"
+        textLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        textLabel.textColor = .red
+        textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
     }
 }
 
